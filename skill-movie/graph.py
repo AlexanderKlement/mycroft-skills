cb138fa4b1ad0b1ca8c25cde0245596462eded93
Michaela Egger <michaela.egger4@gmail.com>
2019-01-19 23:02:33 +0100

    Added release-date, description and is actor/director (part)

--- skill-movie/graph.py
+++ skill-movie/graph.py
@@ -88,3 +88,86 @@ class Graph:
         sparql.setQuery(qt.substitute(graph_link=GRAPH_LINK))
         sparql.setReturnFormat(JSON)
         return sparql.query().convert()
+
+
+    @staticmethod
+    def get_release_by_movie_name(movie):
+        sparql = SPARQLWrapper(WRAPPER)
+        qt = Template("""
+                    PREFIX schema: <http://schema.org/>
+                    SELECT *
+                    FROM <"$graph_link">
+                    WHERE 
+                    {
+                        ?movie a schema:Movie.
+                        ?movie schema:name "$movie_name".
+                        ?movie schema:datePublished ?datePublished 
+                    } 
+                    """)
+        sparql.setQuery(qt.substitute(movie_name=movie))
+        sparql.setQuery(qt.substitute(graph_link=GRAPH_LINK))
+        sparql.setReturnFormat(JSON)
+        return sparql.query().convert()
+
+    @staticmethod
+    def get_description_by_movie_name(movie):
+        sparql = SPARQLWrapper(WRAPPER)
+        qt = Template("""
+                    PREFIX schema: <http://schema.org/>
+                    SELECT *
+                    FROM <"$graph_link">
+                    WHERE 
+                    {
+                        ?movie a schema:Movie.
+                        ?movie schema:name "$movie_name".
+                        ?movie schema:description ?description
+                    } 
+                    """)
+        sparql.setQuery(qt.substitute(movie_name=movie))
+        sparql.setQuery(qt.substitute(graph_link=GRAPH_LINK))
+        sparql.setReturnFormat(JSON)
+        return sparql.query().convert()
+
+
+    @staticmethod
+    def get_if_actor_by_movie_name_and_actor(actor,movie):
+        sparql = SPARQLWrapper(WRAPPER)
+        qt = Template("""
+                    PREFIX schema: <http://schema.org/>
+                    SELECT *
+                    FROM <"$graph_link">
+                    WHERE 
+                    {
+                        ?movie a schema:Movie.
+                        ?movie schema:name "$movie_name".
+                        ?movie schema:actor ?actor.
+                        ?actor schema:name "$actor_name"
+                    } 
+                    """)
+        sparql.setQuery(qt.substitute(movie_name=movie))
+        sparql.setQuery(qt.substitute(actor_name=actor))
+        sparql.setQuery(qt.substitute(graph_link=GRAPH_LINK))
+        sparql.setReturnFormat(JSON)
+        return sparql.query().convert()
+
+
+    @staticmethod
+    def get_if_director_by_movie_name_and_director(director,movie):
+        sparql = SPARQLWrapper(WRAPPER)
+        qt = Template("""
+                    PREFIX schema: <http://schema.org/>
+                    SELECT *
+                    FROM <"$graph_link">
+                    WHERE 
+                    {
+                        ?movie a schema:Movie.
+                        ?movie schema:name "$movie_name".
+                        ?movie schema:director ?director.
+                        ?director schema:name "$director_name"
+                    } 
+                    """)
+        sparql.setQuery(qt.substitute(movie_name=movie))
+        sparql.setQuery(qt.substitute(director_name=director))
+        sparql.setQuery(qt.substitute(graph_link=GRAPH_LINK))
+        sparql.setReturnFormat(JSON)
+        return sparql.query().convert()
